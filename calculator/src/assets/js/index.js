b3033667d73dd3ef2093d63a5f93fe808fbb19a5
Moriyama Masataka <masataka.312@gmail.com>
2019-11-12 23:43:20 +0900

    render provider

--- calculator/src/assets/js/index.js
+++ calculator/src/assets/js/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './page/App';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
 import { Provider, connect } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
