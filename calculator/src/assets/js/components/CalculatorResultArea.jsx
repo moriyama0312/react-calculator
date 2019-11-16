7c98bfbc583bbeb38726bd34be0e9d0c416d689e
Moriyama Masataka <masataka.312@gmail.com>
2019-11-16 14:26:33 +0900

    result area

---
+++ calculator/src/assets/js/components/CalculatorResultArea.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class CalculatorResultArea extends React.Component {
+	render() {
+		return (
+			<div className="calculator-result">
+				<div className="calculator-result__inner">
+					<span>0</span>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default CalculatorResultArea;
