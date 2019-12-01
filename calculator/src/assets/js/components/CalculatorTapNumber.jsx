32662040bdc682ed5b1dd2435820749a736a22fd
Moriyama Masataka <masataka.312@gmail.com>
2019-12-01 13:43:37 +0900

    data-type追加

--- calculator/src/assets/js/components/CalculatorTapNumber.jsx
+++ calculator/src/assets/js/components/CalculatorTapNumber.jsx
@@ -4,20 +4,20 @@ class CalculatorTapNumber extends React.Component {
 	render() {
 		return (
 			<ul className="calculator-tap__numbers">
-				<li className="number others">AC</li>
-				<li className="number others">+/-</li>
-				<li className="number others">%</li>
-				<li className="number">7</li>
-				<li className="number">8</li>
-				<li className="number">9</li>
-				<li className="number">4</li>
-				<li className="number">5</li>
-				<li className="number">6</li>
-				<li className="number">1</li>
-				<li className="number">2</li>
-				<li className="number">3</li>
-				<li className="number double-column">0</li>
-				<li className="number">.</li>
+				<li className="others" data-other-type="ac">AC</li>
+				<li className="others" data-other-type="inversion">+/-</li>
+				<li className="others" data-other-type="remainder">%</li>
+				<li className="number" data-num="7">7</li>
+				<li className="number" data-num="8">8</li>
+				<li className="number" data-num="9">9</li>
+				<li className="number" data-num="4">4</li>
+				<li className="number" data-num="5">5</li>
+				<li className="number" data-num="6">6</li>
+				<li className="number" data-num="1">1</li>
+				<li className="number" data-num="2">2</li>
+				<li className="number" data-num="3">3</li>
+				<li className="number double-column" data-num="0">0</li>
+				<li className="dot" data-num=".">.</li>
 			</ul>
 		);
 	}
--- calculator/src/assets/js/components/CalculatorTapOperator.jsx
+++ calculator/src/assets/js/components/CalculatorTapOperator.jsx
@@ -4,11 +4,11 @@ class CalculatorTapOperator extends React.Component {
 	render() {
 		return(
 			<ul className="calculator-tap__operators">
-				<li className="operator">÷</li>
-				<li className="operator">×</li>
-				<li className="operator">-</li>
-				<li className="operator">+</li>
-				<li className="operator">=</li>
+				<li className="operator" data-op-type="div">÷</li>
+				<li className="operator" data-op-type="multi">×</li>
+				<li className="operator" data-op-type="subtraction">-</li>
+				<li className="operator" data-op-type="addition">+</li>
+				<li className="operator" data-op-type="equal">=</li>
 			</ul>
 		);
 	}
